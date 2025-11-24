35a4c506ae25fcdab10da5228be6dbe03f9f5b5d
RutKoren <Ruth-koren@gmail.com>
2025-11-25 00:07:52 +0200

    convert the json data to Tasks List

--- src/model/TaskRepository.java
+++ src/model/TaskRepository.java
@@ -1,13 +1,14 @@
 package model;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TaskRepository {
 
 	private final String JSON_FILE = "C:/Users/java/TaskManagement/src/model/Tasks.json";
-	private List<Task> tasks;
+	private List<Task> tasks  = new ArrayList<Task>();
 	
 	
 	public List<Task> ListAll()
@@ -26,6 +27,40 @@ public class TaskRepository {
           data = data.replace("}", "");
             
           String [] d =  data.split(",");
+          Task t = new Task();;
+          for(String field : d)
+          {
+       	  String current [] = field.split(":");
+       	  switch(current[0])
+       	  {
+       	  case "id":
+       		  t = new Task();
+       		  t.setId(Integer.parseInt(current[1]));
+       	      break;
+       	  case "title":
+       		  t.setTitle(current[1]);
+       		  break;
+       	  case "description":
+       		  t.setDescription(current[1]);
+       		  break;
+       	  case "status":
+       		  switch(current[1])
+       		  {
+       		  case " DONE":
+       			  t.setStatus(Status.DONE);
+       			  break;
+       		  case " IN_PROGRESS":
+       			  t.setStatus(Status.IN_PROGRESS);
+       			  break;
+       		  case " NEW":
+           		  t.setStatus(Status.NEW);
+           		  break;
+       		  }
+       		  tasks.add(t);
+       		  break;
+       	  }
+          }
+           
             
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred: File not found.");
