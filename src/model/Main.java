72081391727a22850311e671ab1d18d5fbd6d10f
RutKoren <Ruth-koren@gmail.com>
2025-11-24 23:38:36 +0200

    create TaskRepository class and json file

--- src/model/Main.java
+++ src/model/Main.java
@@ -3,15 +3,7 @@ package model;
 public class Main {
 
 	public static void main(String[] args) {
-		TaskRepository t = new TaskRepository();
-		try
-		{
-			t.ListAll();
-		}
-	 catch(Exception e)
-		{
-		 System.out.println(e);
-		}
+		
 		
 	}
 
